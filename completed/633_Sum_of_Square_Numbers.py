66765c97109b3809c81581ca3f4ec04386441d29
kamaraj1226 <kamaraj1226@gmail.com>
2024-06-17 09:50:32 +0530

    completed 17062024

---
+++ completed/633_Sum_of_Square_Numbers.py
@@ -0,0 +1,44 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=use-dict-literal
+# pylint: disable=invalid-name
+# pylint: disable=import-outside-toplevel
+
+
+def solution(c):
+
+    import math
+
+    r = int(math.sqrt(c))
+    l = 0
+
+    while l <= r:
+        _t = l * l + r * r
+        if _t == c:
+            return True
+
+        if _t > c:
+            r -= 1
+        else:
+            l += 1
+
+    return False
+
+
+def main():
+    test_cases = [
+        dict(c=5, output=True),
+        dict(c=3, output=False),
+        dict(c=4, output=True),
+        dict(c=6, output=False),
+        dict(c=1000, output=True),
+    ]
+    for i, test_case in enumerate(test_cases):
+        c, required_output = test_case.values()
+        output = solution(c)
+
+        print(f"Test {i+1}: {'pass' if output== required_output else 'failed'}")
+
+
+if __name__ == "__main__":
+    main()
